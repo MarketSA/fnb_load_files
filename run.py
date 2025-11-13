97c4b28f4fc70018c5b665358e2cbaed467dc4f2
Given Kafuwa <gkafuwa@gmail.com>
2025-11-13 14:14:09 +0200

    Add error messages

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os, sys, requests
 from datetime import datetime
-from data import log, get_campaigns, process_xlsx, process_csv, check_duplicate_data_nopop, 
+from data import log, get_campaigns, process_xlsx, process_csv, check_duplicate_data_nopop
 from fnb import fnb_process_data
 
 
@@ -23,12 +23,12 @@ def process_from_folder():
     message = ""
     status=  'fail'
     try:
-        print('Start running', {"id": 1})
+        print('Start running')
 
-        camp_find = get_campaigns({"id": 1})
+        camp_find = get_campaigns()
         if camp_find:
             for i in camp_find['files']:
-                message += 
+                message += f"{i['name']}\n"
                 file_path = f"{i['folder']}\\{i['fileName'].replace('<YYYYMMDD>', datetime.now().strftime('%Y%m%d'))}"
                 if os.path.exists(file_path):
                     print("list_files", file_path)
@@ -57,11 +57,11 @@ def process_from_folder():
                         # file_data = check_duplicate_res['og_data']
                         # res['dupes'] += check_duplicate_res['dupe_data']
                         # res['dupes_infile'] += len(check_duplicate_res['dupe_data'])
-            
+                    message += f"Total records to process: {len(file_data)}\n"
                     return
 
                     data_res, data_code = fnb_process_data(file_data)
-                    
+                    message += f"{data_res}\n \n"
                     if data_code == 200:
                         try:
                             # os.remove(f'{folder}/{i}')
@@ -69,33 +69,22 @@ def process_from_folder():
                         except Exception as e:
                             log('[Error in delete file from process_from_folder ]', f"{e} on line> {e.__traceback__.tb_lineno}")
                     else:
-                        data_res['error'] = f'File {i} processed with errors'
-
-                    data_res['file_name'] = f"{i}"
-                    data_res['camp_name'] = f"{camp_find['name']}"
-                    res['files'].append(data_res)
+                        message += f'File {i['folder']}\\{i['fileName']} processed with errors'
                 else:
-                    print(f'{file_path} does not exist')
-                res['message'] = "process completed"
-                res['status'] = "success"
-                code = 200
-            else:
-                res = {
-                    "message": f"No files found for campaign in the folder: {i['folder']}",
-                    "status": 'fail'
-                }
-                code = 404
+                   message +- f'{file_path} does not exist'
+
+                message = "process completed"
+                status = "success"
+
         else:
-            res['message'] = 'No information found on the campaign'
-            res['status'] = 'fail'
+            message = 'No information found on the campaign'
+            status = 'fail'
             code = 404
     except Exception as e:
-        log('[Error in process_from_folder ]', f"{e} on line> {e.__traceback__.tb_lineno}")
-        res = {
-            "message": f"{e}",
-            "status": 'error*'
-        }
-        code = 500
+        message = '[Error in process_from_folder ]' +  f"{e} on line> {e.__traceback__.tb_lineno}"
+        status = 'fail'
+
+    send_ntfy("FNB Load Files Process", message, tags=status)
     return res, code
 
 
